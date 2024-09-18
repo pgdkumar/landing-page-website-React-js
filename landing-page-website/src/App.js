a40080e8a26d05d2fadd60c702e339628cb3678f
pgdkumar <pgdkumar161@gmail.com>
2024-09-18 16:52:23 +0530

    setup Route

--- landing-page-website/src/App.js
+++ landing-page-website/src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={""}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
