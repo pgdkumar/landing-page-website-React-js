582f92001775fd607ba1681cf7f9c7301620948b
pgdkumar <pgdkumar161@gmail.com>
2024-09-18 16:55:27 +0530

    Create a New Component

--- landing-page-website/src/App.js
+++ landing-page-website/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={""}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
